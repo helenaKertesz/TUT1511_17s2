2b66e03844b958a5aada2120192b790ae21cb26a
helenaKertesz <helena.kertesz1@gmail.com>
2017-07-25 10:14:18 +1000

    changed to Andrew's Hello World

--- tut01/helloWorld.c
+++ tut01/helloWorld.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+// Prints out a friendly message.
+// Andrew Bennett <andrew.bennett@unsw.edu.au>
+// 2017-07-24
 
-int main(){
-   printf("Hello World!!\n");
-   return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+
+    // Print out the famous 'hello world' message.
+    printf ("Hello, world!\n");
+
+    return EXIT_SUCCESS;
 }
