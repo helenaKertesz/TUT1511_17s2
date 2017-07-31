20e8430844a712b08b5ebf4add4fbf6c8eac6ec2
helenaKertesz <helena.kertesz1@gmail.com>
2017-07-31 21:01:37 +1000

    tut2 stubs

---
+++ tut02/leapYear.c
@@ -0,0 +1,14 @@
+// Given a year, print out if it is a leap year
+// Helena Kertesz (z5059868)
+// Tutor: Cadel Watson (W09A)
+// Created: 31/07/2017
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main( int argc, char *argv[]){
+
+    // control flow
+
+    return EXIT_SUCCESS
+}
---
+++ tut02/weekdays.c
@@ -0,0 +1,16 @@
+// Takes in a number from 0-6 and print out
+// the corosponding weekday
+// Helena Kertesz (z5059868)
+// Tutor: Peter Kerr (T15B)
+// Created 31/07/2017
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char* argv[]){
+
+    // using scanf
+
+    //style for if ... else if ... else
+
+}
