88c4aaea812d2bf986682dfec5d514f060c17453
helenaKertesz <helena.kertesz1@gmail.com>
2017-10-19 17:25:32 +1100

    tut9 complete

--- tut09/Card.c
+++ tut09/Card.c
@@ -1,51 +1,40 @@
-nclude "Card.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stderr.h>
+#include "Card.h"
 
-// The following typedef is included in Card.h:
-//         typedef struct _card *Card;
-// This means that a struct card has to be implemented *somewhere*, but
-// not necessarily in the .h file, as we have done with concrete types.
-//
-// The way we implement this in an ADT is that you design your own card
-// struct, with the fields etc that you need to implement your ADT.
-
-typedef struct _card {
-    // Insert the fields here that you need for your
-    // implementation.
+typedef struct _card{
+    char number;
+    color color;
+    suit suit;
 } card;
 
-// Place the prototypes for your own functions here.
-// Don't forget to make them static!
-
-
-
-// The interface functions are below. You need to implement these.
-
-// Create a new card.
-// These values can only be set at creation time.
-// The number should be between 0x0 and 0xF.
 Card newCard(char number, color color, suit suit) {
-    return NULL; // Replace this with your own code.
+    Card cs = calloc(1, sizeof(card));
+    
+    if (cs == NULL) {
+        fprintf(stderr, "Couldn't allocate memory for card\n"); 
+        exit(EXIT_FAILURE);
+    }
+    
+    cs->number = number;
+    cs->color = color;
+    cs->suit = suit;
+    
+    return cs;
 }
 
-// Destroy an existing card.
-void destroyCard(Card card) {
-    return; // Replace this with your own code.
-}
-
-// Get the card suit (HEARTS, DIAMONDS, etc).
 suit cardSuit(Card card) {
-    return HEARTS; // Replace this with your own code.
+    if (card == NULL) {
+        fprintf(stderr, "Invalid pointer\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return card->suit;
 }
 
-// Get the card number (0x0 through 0xF).
-char cardNumber(Card card) {
-    return 0x0; // Replace this with your own code.
+// A white box test
+void testCard() {
+    Card c = newCard(4, PURPLE, CLUBS);
+    assert(c->color == PURPLE);
 }
-
-// Get the card's color (RED, BLUE, etc).
-color cardColor(Card card) {
-    return RED; // Replace this with your own code.
-}
-
-
-// Implement your own functions here, don't forget to make them static!
--- tut09/testCard.c
+++ tut09/testCard.c
@@ -2,8 +2,14 @@
 
 #include<stdio>
 #include<stdlib>
+#include <assert>
+#include "Card.h"
 
-int main(void){
+int main(int argc, char* argv[]){
 
+    // a simple blackbox test
+    Card c = newCard(4, PURPLE, CLUBS);
+    assert(cardSuit(c) == CLUBS);
+    
     return EXIT_SUCCESS;
 }
